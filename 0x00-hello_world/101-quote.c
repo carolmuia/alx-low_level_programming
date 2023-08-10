85a6af17f19e7e3b1f76f2d0337668f7ef71e591
carolmuia <carolmuia60@gmail.com>
2023-08-10 11:19:46 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints a line to standard error
+ * main - program that prints size of various computer types
  * Return: 1 (print)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
