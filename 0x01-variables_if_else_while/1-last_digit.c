2fc1071527c41bbc22c7d42d1c83f2d899fa47f3
carolmuia <carolmuia60@gmail.com>
2023-08-13 08:00:18 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,6 @@ int main(void)
 	else if (m == 0)
 		printf("last digit of %d is %d and is 0\n", n, m);
 	else if (m < 6 && m != 0)
-		printf("last digit of %d is %d and is less than 6 and is not o\n", n, m);
+		printf("last digit of %d is %d and is less than 6 and is not 0\n", n, m);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - task 2
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+char i;	
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
