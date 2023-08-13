380369759026bfd592bc98cf48b24674791c16d4
carolmuia <carolmuia60@gmail.com>
2023-08-13 09:08:10 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints single digits
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
