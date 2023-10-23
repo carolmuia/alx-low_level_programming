1b2855f37f737937df069c21cb2dc736801312c7
carolmuia <carolmuia60@gmail.com>
2023-10-23 10:13:37 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,7 +7,7 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int byted = 0;
+	unsigned int bytes = 0;
 	int index;
 
 	while (*s)
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 			if (*s == accept[index])
 			{
 				bytes++;
-				brak;
+				break;
 			}
 			else if (accept[index + 1] == '\0')
 				return (bytes);
